f73e3190708185a0052691e1e5f37160b441cde0
Jacek Jankowski <jacek143@gmail.com>
2019-12-04 23:18:35 +0000

    Normalize error calculation

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ def error(sensors_value):
     weights = [-4, -3, -2, -1, 1, 2, 3, 4]
     weighted = [weights[ind]*value for ind, value in enumerate(sensors_value) if value]
     if len(weighted):
-        return sum(weighted)/len(weighted)
+        return sum(weighted)/(4*len(weighted))
     return None
 
+def controller(error_value):
+    speed = 20
+    k = 10
+    return (speed+k*error_value,speed-k*error_value)
+
 from sensors_array import SensorsArray
+from motor_driver import MotorDriver
 
 sensors = SensorsArray()
+motor_driver = MotorDriver()
 while(True):
-    print(error(sensors.value()))
+    error_value = error(sensors.value())
+    print(error_value)
