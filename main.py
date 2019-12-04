c2611d3d1ce4a86efd61e0baf6efed6ffd0ee27f
Jacek Jankowski <jacek143@gmail.com>
2019-12-04 23:44:48 +0000

    Implement lf

--- main.py
+++ main.py
@@ -6,15 +6,32 @@ def error(sensors_value):
     return None
 
 def controller(error_value):
+    if error_value is None:
+        return None
     speed = 20
     k = 10
     return (speed+k*error_value,speed-k*error_value)
 
+class Motors:
+    def __init__(self, motor_driver):
+        self.__driver = motor_driver()
+    def set(self, speeds):
+        self.__driver.MotorRun(1, 'forward', speeds[0])
+        self.__driver.MotorRun(0, 'forward', speeds[1])
+    def stop(self):
+        self.__driver.MotorStop(0)
+        self.__driver.MotorStop(1)
+        
 from sensors_array import SensorsArray
 from motor_driver import MotorDriver
 
 sensors = SensorsArray()
-motor_driver = MotorDriver()
+motors = Motors(MotorDriver)
 while(True):
     error_value = error(sensors.value())
-    print(error_value)
+    speeds = controller(error_value)
+    print(speeds)
+    if speeds is not None:
+        motors.set(speeds)
+    else:
+        motors.stop()
