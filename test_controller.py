a2c28195238c0e90eebaaeb5d3c733f5606fe58a
Jacek Jankowski <jacek143@gmail.com>
2019-12-05 21:04:52 +0100

    Refactor

--- test_controller.py
+++ test_controller.py
@@ -1,13 +1,10 @@
 import pytest
 from controller import Controller
 
-def test_if_robot_goes_straight_if_line_in_the_middle():
-    speed = 30
-    controller = Controller(speed)
-    error = 0
-    assert (speed,speed) == controller.process(error)
+def test_if_goes_straight_with_given_speed_if_line_in_the_middle():
+    controller = Controller(speed=25)
+    assert (25,25) == controller.process(0)
 
 def test_if_does_not_start_when_no_line():
-    speed = 20
-    controller = Controller(speed)
+    controller = Controller(speed=10)
     assert (0, 0) == controller.process(None)
